821692b0cb6e8e23c3504b65c91dc6aa8ec584f2
Adebiyi07 <adebiyijohn07@gmail.com>
2023-12-15 14:55:14 -0800

    Get line functions

---
+++ get_line.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * gets_line - reads input from standard input
+ *
+ * Return: string inputed
+*/
+
+void *gets_line(void)
+{
+	static int current_position, buffer_size;
+	static char buffer[BUFFER_SIZE];
+	char *user_input = NULL;
+	char current_char;
+	int input_length = 0;
+
+	while (1)
+	{
+		if (current_position >= buffer_size)
+		{
+			buffer_size = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+			current_position = 0;
+			if (buffer_size == 0)
+				return (user_input);
+			else if (buffer_size < 0)
+			{
+				perror("read");
+				return (NULL);
+			}
+		}
+		current_char = buffer[current_position];
+		current_position++;
+
+		if (current_char == '\n')
+		{
+			user_input = realloc(user_input, input_length + 1);
+			user_input[input_length] = '\0';
+			return (user_input);
+		}
+		else
+		{
+			user_input = realloc(user_input, input_length + 1);
+			user_input[input_length] = current_char;
+			input_length++;
+		}
+	}
+}
